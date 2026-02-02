20e892131d4834bd4faaa9e12b393048cc9fab9a
Kasen659 <kasen.thomas@ucas-edu.net>
2026-02-02 13:44:25 -0700

    doone

---
+++ notes/intergers_floats.py
@@ -0,0 +1,13 @@
+"""price = float(input("How much did it cost: $"))
+
+tax_rate = float(input("What is the tax percent: %"))/100
+
+total_price = price * (1 + tax_rate)
+
+print(round(total_price, 2))"""
+
+apples = 27
+
+friends = 8
+
+print("Each friend gets", apples//friends "and i get", apples%friends, "left over ")
---
+++ notes/loops.py
@@ -0,0 +1,18 @@
+import random #libary 
+start = 0
+
+while start <= 10:
+    print(start)
+    start += 1
+
+print("the loop is over")
+
+goose = random.randint(1,26)
+count = 1
+
+while count != goose:
+    print("Duck")
+    count += 1
+
+print(goose)
+
