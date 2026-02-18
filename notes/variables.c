96619ccb08811f1e68d306576ad23d82c0b8421c
Kasen659 <kasen.thomas@ucas-edu.net>
2026-02-18 13:43:13 -0700

    hiii

---
+++ notes/variables.c
@@ -0,0 +1,14 @@
+// this is how you write a comment in C
+#include <stdio.h> // lets you write inputs and outputs
+
+int main(){
+    int number = 8;
+    float pi = 3.14;
+    char grade = 'A';
+    char name[] = "kasen";
+    printf("working!\n");
+    printf("%d\n", number);
+    printf("the digets of pi are: %f\n", pi);
+    printf("your name is %s you are %d old. You have an %c in programing pi is %f\n",name ,number ,grade, pi);
+    return 0;
+}
