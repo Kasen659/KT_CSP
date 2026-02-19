e7cf4e42cd84063603c78775443896f8b1a2deaf
Kasen659 <kasen.thomas@ucas-edu.net>
2026-02-19 13:44:00 -0700

    variables

---
+++ practices/variables.c
@@ -0,0 +1,13 @@
+//Kasen Thomas p.6 variables 
+#include <stdio.h>
+
+int main(){
+    char name = "Kasen";
+    int number = 8;
+    const float GPA = 3.14;
+    char breakfast = "Pancakes";
+    char color = "Blue";
+    char school = "Ucas";
+    int year = 2026;
+    char eye = "Gray";
+    int year
