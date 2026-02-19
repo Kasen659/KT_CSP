76289f15091278e25a5b4b8a739bb245afaf2c5f
Kasen659 <kasen.thomas@ucas-edu.net>
2026-02-19 13:16:53 -0700

    notes

--- notes/variables.c
+++ notes/variables.c
@@ -3,12 +3,21 @@
 
 int main(){
     int number = 8;
-    float pi = 3.14;
+    const float pi = 3.14;
     char grade = 'A';
     char name[] = "kasen";
+    char user[50];
+
+    printf("Tell me your name\n");
+    scanf("%s", &user);
+
+
+    printf("Tell me how old you are:\n");
+    scanf("%d", &number);
+
     printf("working!\n");
     printf("%d\n", number);
     printf("the digets of pi are: %f\n", pi);
-    printf("your name is %s you are %d old. You have an %c in programing pi is %f\n",name ,number ,grade, pi);
+    printf("your name is %s you are %d old. You have an %c in programing pi is %f\n",user ,number ,grade, pi);
     return 0;
 }
