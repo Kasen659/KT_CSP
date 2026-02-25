6a7bf3cd833bdab980cb569191621ccd85497038
Kasen659 <kasen.thomas@ucas-edu.net>
2026-02-25 13:41:35 -0700

    silly

---
+++ practices/silly_sentince.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    char person[20];
+    int age;
+    char action[20];
+    char sentince[100];
+
+    printf("Name a person in the room: ");
+    scanf("%s", &person);
+
+    printf("An action that you want them to do: ");
+    scanf("%s", &action);
+    
+    printf("Give them an age: ");
+    scanf("%d", &age);
+    
+    strcat(sentince,"my name is " );
+    str
+    
+    printf("My name is %s and im %d years old. And i like to %s.\n", person, age, action);
+    
+    return 0;
+}
