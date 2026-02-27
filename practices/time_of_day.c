ecf24fb3f42f831f1e9d05bde01d9fe081ae8fcc
Kasen659 <kasen.thomas@ucas-edu.net>
2026-02-27 13:41:54 -0700

    time of day

---
+++ practices/time_of_day.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    int time = 0;
+
+    printf("what time of day is it in milatary time: ");
+    scanf("%d", &time);
+
+    if (time >= 16){
+        printf("Good evening\n")
+    }else if  
+        
+    
+
+
+
+
+    return 0;
+}
