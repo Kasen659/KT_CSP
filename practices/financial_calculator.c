3ccf3ab55d7134061b948473c91eda54bb266ecc
Kasen659 <kasen.thomas@ucas-edu.net>
2026-03-06 13:35:11 -0700

    done with finance

--- practices/financial_calculator.c
+++ practices/financial_calculator.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 int main(){
-    float income = 0;
-    float rent = 0;
-    float utilities = 0;
-    float groceries = 0;
-    float transport = 0;
-
+    float income;
+    float rent;
+    float utilities;
+    float groceries;
+    float transport;
+    
+    
+    
     printf("\nWhat is your monthly income:");
     scanf("%f", &income);
 
@@ -21,8 +23,16 @@ int main(){
 
     printf("\nWhat do you spend montly on transport:");
     scanf("%f", &transport);
+    float monthly_rent = (rent / income* 100); 
+    printf("\nYou spend %.2f on rent that is %.1f percent of your income", rent, monthly_rent);
+    float monthly_utilities = (utilities / income* 100);
+    printf("\nYou spend %.2f on utilities that is %.1f percent of your income", utilities, monthly_utilities);
+    float monthly_groceries = (groceries / income* 100);
+    printf("\nYou spend %.2f on groceries that is %.1f percent of your income", groceries, monthly_groceries);
+    float monthly_transport = (transport / income* 100);
+    printf("\nYou spend %.2f on transport that is %.1f percent of your income", transport, monthly_transport);
+    float save = (income * 0.1);
+    printf("\n you should save %.2f that is 10 percent of your income", save);
 
-    printf("\n");
-   
     return 0;
 }    
